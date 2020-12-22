e63a03d79e54a87c18e70d3485d48c85ef3d4b39
Teresia Gable <45754965+teresiagable@users.noreply.github.com>
2020-12-22 14:11:33 +0100

    base application

---
+++ MVCData_assignments/Controllers/PersonController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCData_assignments.Models;
+
+namespace MVCData_assignments.Controllers
+{
+    public class PersonController : Controller
+    {
+
+        IPersonService _personService;
+        PersonListViewModel personListVM = new PersonListViewModel();
+
+        public PersonController()
+        {
+            _personService = new PersonService();
+        }
+
+        public IActionResult Index()
+        {
+            personListVM.PersonList = _personService.GetAll();
+            return View(personListVM);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(PersonCreateModel personVM)
+        {
+            if (ModelState.IsValid)
+            {
+                personListVM.PersonList = _personService.CreatePerson(personVM.Name, personVM.City, personVM.Phonenumber);
+                return RedirectToAction("Index", personListVM);
+
+            }
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Update(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                _personService.UpdatePerson(person);
+                return PartialView("_personPartial", person);
+
+            }
+
+            return View();
+        }
+
+
+        public IActionResult Edit(int id)
+        {
+            return PartialView("_EditPersonPartial", _personService.GetById(id));
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            if (_personService.RemovePerson(id))
+            {
+                personListVM.PersonList = _personService.GetAll();
+                ViewBag.DeleteMsg = "Successfully deleted";
+            }
+            else ViewBag.DeleteMsg = "Delete failed";
+            return View("Index", personListVM);
+        }
+
+        [HttpPost]
+        public IActionResult Filter(string filter)
+        {
+            personListVM.Filter = filter;
+            personListVM.PersonList = _personService.FilterPerson(filter);
+            return View("Index", personListVM);
+        }
+    }
+}
---
+++ MVCData_assignments/Models/PersonService.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVCData_assignments.Models;
+
+namespace MVCData_assignments.Models
+{
+    public class PersonService : IPersonService
+    {
+        private static int PersonId = 0;
+
+        private static List<Person> personList = new List<Person>();
+
+
+        public PersonService()
+        {
+            //personList.Add(new Person("Anna Andersson", "Anderstorp", "111-2222333"));
+            //personList.Add(new Person("Bosse Bildoktorn", "Bollebygd", "222-223344"));
+            //personList.Add(new Person("Ceasar Carlsson", "Cöpenhamn", "333-2224455"));
+            //personList.Add(new Person("Daniel Doppsko", "Sålundastad", "444-2255667"));
+        }
+
+        public List<Person> GetAll()
+        {
+            return personList;
+        }
+
+        public Person GetById(int id)
+        {
+            //should be some error handling here
+            return personList.Find(p => p.Id == id);
+        }
+
+        public bool UpdatePerson(Person person)
+        {
+            Person oldPerson = personList.FirstOrDefault(p => p.Id == person.Id);
+            if (oldPerson != null)
+            {
+                oldPerson.Name = person.Name;
+                oldPerson.City = person.City;
+                oldPerson.Phonenumber = person.Phonenumber;
+                return true;
+            }
+            return false;
+        }
+
+
+        public List<Person> CreatePerson(string name, string city, string phonenumber)
+        {
+            int newId = ++PersonId;
+            Person per = new Person() { Id = newId, Name = name, City = city, Phonenumber = phonenumber };
+
+            personList.Add(per);
+            return personList;
+        }
+
+        public bool RemovePerson(int id)
+        {
+            Person per = personList.Find(p => p.Id == id);
+            return personList.Remove(per);
+        }
+
+        public List<Person> FilterPerson(string filterP)
+        {
+            List<Person> filteredList = new List<Person>();
+            List<Person> returnList = new List<Person>();
+
+            if (!string.IsNullOrEmpty(filterP))
+                filteredList = personList.FindAll(p => p.Name.ToLower().Contains(filterP.ToLower()));
+            returnList = filteredList;
+
+            if (!string.IsNullOrEmpty(filterP))
+                filteredList = personList.FindAll(p => p.City.ToLower().Contains(filterP.ToLower()));
+
+            var templist = returnList.Concat(filteredList);
+            returnList = templist.ToList();
+
+            if (!string.IsNullOrEmpty(filterP))
+                filteredList = personList.FindAll(p => p.Phonenumber.ToLower().Contains(filterP.ToLower()));
+
+            templist = returnList.Concat(filteredList);
+            returnList = templist.Distinct().ToList();
+
+            return returnList;
+        }
+    }
+}
